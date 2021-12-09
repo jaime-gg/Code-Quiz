2fe1d473898250232f255e919eb9cdba89ed1b18
jester <jaimebuddy98@gmail.com>
2021-12-09 15:53:06 -0600

    took array contents and linked them to my page elements to loop through questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,64 @@
+// array with questions/choices/answers to loop through later. 
+var quizQuestions = [
+    {
+        title: "Commonly used data types DO NOT include:",
+        choices : [
+           "strings",
+           "booleans",
+           "alert",
+           "numbers"
+        ],
+        answer : "alert"
+    },
+
+    {
+        title: "The condition in an if / else statement is enclosed within _____.",
+        choices : [
+           "quotes",
+           "curly brackets",
+           "parentheses",
+           "square brackets"
+        ],
+        answer : "parentheses"
+    },
+
+    {
+        title: "Arrays in JavaScript can be used to store _____.",
+        choices : [
+           "numbers and strings",
+           "other arrays",
+           "booleans",
+           "all of the above"
+        ],
+        answer : "all of the above"
+    },
+
+    {
+        title: "String values must be enclosed within _____ when being assigned to variables.",
+        choices : [
+           "commas",
+           "curly brackets",
+           "quotes",
+           "parentheses"
+        ],
+        answer : "quotes"
+    },
+
+    {
+        title: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        choices : [
+           "JavaScript",
+           "terminal / bash",
+           "for loops",
+           "console log"
+        ],
+        answer : "console log"
+    }
+];
+
 // todo 
     // 0. setup quiz questions and answers
-    // 1. click start button
+    // 1. click start button 
     //      1.1. start timer
     //      1.2. hide initial page
     //      1.3. show page with question
@@ -28,68 +86,9 @@ var choicesEl = document.querySelector("#choices");
 var initialsEl = document.querySelector("#initials");
 var submitBtn = document.querySelector("#submit");
 
+var currentQuestionIndex = 0;
 var time = 75;
-var timerId;
-
-// quiz questions and answers
-var questions = [
-    question1 = {
-        textContent: "Commonly used data types DO NOT include:",
-        options : [
-           "strings",
-           "booleans",
-           "alert",
-           "numbers"
-        ],
-        answer : "alert"
-    },
-
-    question2 = {
-        textContent: "The condition in an if / else statement is enclosed within _____.",
-        options : [
-           "quotes",
-           "curly brackets",
-           "parentheses",
-           "square brackets"
-        ],
-        answer : "parentheses"
-    },
-
-    question3 = {
-        textContent: "Arrays in JavaScript can be used to store _____.",
-        options : [
-           "numbers and strings",
-           "other arrays",
-           "booleans",
-           "all of the above"
-        ],
-        answer : "all of the above"
-    },
-
-    question4 = {
-        textContent: "String values must be enclosed within _____ when being assigned to variables.",
-        options : [
-           "commas",
-           "curly brackets",
-           "quotes",
-           "parentheses"
-        ],
-        answer : "quotes"
-    },
-
-    question5 = {
-        textContent: "A very useful tool used during development and debugging for printing content to the debugger is:",
-        options : [
-           "JavaScript",
-           "terminal / bash",
-           "for loops",
-           "console log"
-        ],
-        answer : "console log"
-    }
-];
-
-function tickTok() {
+function setupTimer() {
     // update time
     time--;
     timerEl.textContent = time;
@@ -101,24 +100,85 @@ function tickTok() {
   }
 
 function startQuiz() {
+
     startScreenEl.setAttribute("class", "hide")
     questionsEl.removeAttribute("class");
 
-    timerId = setInterval(tickTok, 1000);
+    timerId = setInterval(setupTimer, 1000);
     timerEl.textContent = time;
 
-    askQuestion();
+    getQuestion();
 }
 
-function askQuestion() {
-    var currentQuestion = questions[currentQuestionIndex];
+
+
+function getQuestion() {
+    var currentQuestion = quizQuestions[currentQuestionIndex];
+  
+    var titleEl = document.getElementById("question-title");
+    titleEl.textContent = currentQuestion.title;
+  
+    choicesEl.innerHTML = "";
+  
+    // loop 
+    currentQuestion.choices.forEach(function(choice, i) {
+      // new button for each choice
+      var choiceNode = document.createElement("button");
+      choiceNode.setAttribute("class", "choice");
+      choiceNode.setAttribute("value", choice);
+  
+      choiceNode.textContent = i + 1 + ". " + choice;
+  
+      // listener
+      choiceNode.onclick = questionClick;
+      choicesEl.appendChild(choiceNode);
+    });
 }
+
+function questionClick() {
+    if (this.value === quizQuestions[currentQuestionIndex].answer) {
+        feedbackEl.textContent = "Correct!";
+        feedbackEl.style.color = "green";
+        feedbackEl.style.fontSize = "150%";
+        feedbackEl.style.marginTop = "20px";
+    } else {
+      time -= 15;
+      if (time < 0) {
+        time = 0;
+      }
+      timerEl.textContent = time;
+      feedbackEl.textContent = "Wrong!";
+      feedbackEl.style.color = "red";
+      feedbackEl.style.fontSize = "150%";
+      feedbackEl.style.marginTop = "20px";
+      
+    }
+  
+    // fade feedback
+    feedbackEl.setAttribute("class", "feedback");
+    setTimeout(function() {
+      feedbackEl.setAttribute("class", "feedback hide");
+    }, 1000);
+  
+    // next question
+    currentQuestionIndex++;
+  
+    if (currentQuestionIndex === quizQuestions.length) {
+      quizEnd();
+    } else {
+      getQuestion();
+    }
+}
+
+
 
 function quizEnd() {
-    
+    clearInterval(timerId);
+    questionsEl.setAttribute("class", "hide")
+
+    var endScreenEl = document.getElementById("end-screen");
+    endScreenEl.removeAttribute("class");
 }
-
-
 
 // start quiz
 startBtn.onclick = startQuiz;
